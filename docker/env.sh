d94ab35b84cbd71cf5274293d5ad722f42ea03fe
ryotomatsuba <metbyroot@gmail.com>
2021-03-22 17:14:13 +0900

    fix

--- docker/env.sh
+++ docker/env.sh
@@ -2,7 +2,7 @@
 #
 # Set environment variables.
 
-export IMAGE_NAME=${USER}_pytorch_project
-export CONTAINER_NAME=${USER}_pytorch_project
+export IMAGE_NAME=${USER}_object_detection
+export CONTAINER_NAME=${USER}_object_detection
 export HOST_PORT=8888
 export CONTAINER_PORT=8888
